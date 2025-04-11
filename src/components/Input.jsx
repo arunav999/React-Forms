90a0c88cc1900a170f4bd57ca3980abb6d34a8d9
Arunav Singh <arunavsingh999@gmail.com>
2025-04-11 23:32:49 +0530

    building & using a reusable input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
 
-export default function Input() {
+export default function Input({ label, id, error, ...props }) {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <div className="control no-margin">
+        <label htmlFor={id}>{label}</label>
+        <input id={id} {...props} />
+        <div className="control-error">{error && <p>{error}</p>}</div>
+      </div>
+    </>
+  );
 }
